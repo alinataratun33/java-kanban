70da3f8eb7093d074c2d79e7432f49b3e4a6a4f3
Alina <taratunalina@yandex.by>
2025-08-11 01:14:34 +0300

    Удаление лишнего кода в main

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Поехали!");
 
-        Manager manager = new Manager();
-
-        Epic epic1 = manager.createEpic(new Epic("fsf", " e", Status.NEW));
-
-
-        ArrayList<Task> task = manager.getAllTasks();
-
-        System.out.println(task);
-
-
     }
 }
